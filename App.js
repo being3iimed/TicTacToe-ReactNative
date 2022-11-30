881a3fabf472be02708fde20b9044ddd23396f9d
MeeloRock <imed.kouki@rades.r-iset.tn>
2022-11-30 22:40:54 +0000

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from 'react';
+import { 
+  StyleSheet,
+  Text, 
+  View,
+  SafeAreaView, 
+  FlatList,
+  TouchableOpacity, 
+  StatusBar,
+  Image,
+  Dimensions,
+  } from 'react-native';
+import cloneDeep from 'lodash/cloneDeep';
+
+const { height } = Dimensions.get('window');
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.SafeAreaView}/>
+      <Statusbar barStyle={'light-content'}/>
+      <View style={styles.tttContainer}>
+
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  SafeAreaView: {
+    flex: 0,
+    backgroundColor: 'navy',
+  },
+  screenContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  boxContainer: {
+    borderWidth: 5,
+    borderColor: 'darkorange',
+    backgroundColor: 'white',
+    width: 100,
+    height: 100,
+    margin: 15,
+    alignContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+  },
+  tttContainer: {
+    backgroundColor: 'navy',
+    borderBottomWidth: 5,
+    borderBottomColor: 'darkorange',
+  },
+  selection: {
+    fontSize: 50,
+  },
+  resetBottomContainer: {
+    borderTopWidth: 5,
+    borderColor: 'navy',
+    fontWeight: 'bold',
+  },
+  imageContainer: {
+    flex:1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logoStyle: {
+    width: 325,
+    height: 60,
+    bottom: height * 0.05,
   },
 });
